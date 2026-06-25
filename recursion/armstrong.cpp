77f54368ac160f060a19d52d7cab614ba876593b
arynothic <duhoonaryan2006@gmail.com>
2026-06-25 11:49:05 +0530

    armstrong

---
+++ recursion/armstrong.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+bool armstrong(int num, int cnt){
+  int sum=0;
+  while(num>0){
+    sum=sum+pow(num%10,cnt);
+    num=num/10;
+  }
+  return sum;
+}
+
+int digits(int n){
+  int cnt=0;
+  while(n>0){
+    n=n/10;
+    cnt++;
+  }
+  return cnt;
+}
+
+int main(){
+  int n = 153;
+  int cnt = digits(n);
+  bool ans = armstrong(n,cnt);
+  cout<<ans;
+}
+
+
+
